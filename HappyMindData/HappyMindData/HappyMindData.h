1dfc4bb4985bf6f0a6668da6fc4189c42c6313bb
Carlos Pava <cpava@condorlabs.io>
2020-09-26 15:29:21 -0500

    add happy mind data

---
+++ HappyMindData/HappyMindData/HappyMindData.h
@@ -0,0 +1,19 @@
+//
+//  HappyMindData.h
+//  HappyMindData
+//
+//  Created by Carlos Pava on 26/09/20.
+//  Copyright © 2020 Carlos Pava. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HappyMindData.
+FOUNDATION_EXPORT double HappyMindDataVersionNumber;
+
+//! Project version string for HappyMindData.
+FOUNDATION_EXPORT const unsigned char HappyMindDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HappyMindData/PublicHeader.h>
+
+
