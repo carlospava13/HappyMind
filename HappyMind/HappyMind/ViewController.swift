7e704b5ab1b44c4b59ce6bc29ad4739d77b763d5
Carlos Pava <cpava@condorlabs.io>
2020-09-26 15:23:00 -0500

    firts project.

---
+++ HappyMind/HappyMind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyMind
+//
+//  Created by Carlos Pava on 26/09/20.
+//  Copyright © 2020 Carlos Pava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
