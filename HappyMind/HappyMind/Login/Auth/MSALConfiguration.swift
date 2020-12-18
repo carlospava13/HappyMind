29cd94801a7a87a5a22521c7216a7a4b81876b44
carlos pava <cpava@condorlabs.io>
2020-12-18 07:57:12 -0500

    remove comments.

--- HappyMind/HappyMind/Login/Auth/MSALConfiguration.swift
+++ HappyMind/HappyMind/Login/Auth/MSALConfiguration.swift
@@ -11,34 +11,18 @@ import Foundation
 final class MSALConfiguration {
 
     static var kClientID: String {
-        //#if DEBUG
-            return "ed9d844d-39aa-47a1-a904-132a948fee77"
-       // #else
-        //    return "Prod"
-        //#endif
+        return "ed9d844d-39aa-47a1-a904-132a948fee77"
     }
-    
+
     static var kRedirectUri: String {
-        //#if DEBUG
-            return "msauth.co.edu.sena.happymind://auth"
-       // #else
-        //    return "Prod"
-        //#endif
+        return "msauth.co.edu.sena.happymind://auth"
     }
-    
+
     static var kAuthority: String {
-        //#if DEBUG
-            return "https://login.microsoftonline.com/cbc2c381-2f2e-4d93-91d1-506c9316ace7"
-        //#else
-        //    return "Prod"
-        //#endif
+        return "https://login.microsoftonline.com/cbc2c381-2f2e-4d93-91d1-506c9316ace7"
     }
-    
+
     static var kGraphEndpoint: String {
-        //#if DEBUG
-            return "https://graph.microsoft.com/"
-       // #else
-         //   return "Prod"
-        //#endif
+        return "https://graph.microsoft.com/"
     }
 }
